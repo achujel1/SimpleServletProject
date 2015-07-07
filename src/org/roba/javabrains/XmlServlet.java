de6f37f85df0763228192234283ff03483007eb9
achujel1 <achujel1@gmail.com>
2015-07-07 19:46:24 +0300

    Part 6. Passing More Parameters
    Testing more than one parameter passing

--- src/org/roba/javabrains/XmlServlet.java
+++ src/org/roba/javabrains/XmlServlet.java
@@ -9,9 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class XmlServlet extends HttpServlet {
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * Main Servlet method
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
@@ -21,11 +21,24 @@ public class XmlServlet extends HttpServlet {
 		out.println("Hello from GET method, " + userName);
 	}
 
+	/**
+	 * Simple method which is executed
+	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		String userName = request.getParameter("userName");
-		out.println("Hello from POST method, " + userName);
+		String fullName = request.getParameter("fullName");
+		out.println("Hello from POST method, " + userName
+				+ ", your full name: " + fullName);
+		String prof = request.getParameter("prof");
+		out.println("You are a " + prof);
+		String[] location = request.getParameterValues("location");
+		out.println("You are at " + location.length + " places");
+		for (int i = 1; i < location.length; i++) {
+			out.println(location[i]);
+		}
+
 	}
 }
