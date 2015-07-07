43c4ed6767c4c25e69c558f68e0fbc54570e6cf5
achujel1 <achujel1@gmail.com>
2015-07-07 17:17:38 +0300

    Part 4. Servlet XML configuration
    Configured servelt with XML file

---
+++ src/org/roba/javabrains/XmlServlet.java
@@ -0,0 +1,19 @@
+package org.roba.javabrains;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class XmlServlet extends HttpServlet {
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("XML servlet called!");
+	}
+}
