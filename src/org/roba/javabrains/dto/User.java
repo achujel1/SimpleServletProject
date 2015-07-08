28f6e1e0b94edf7c96d23b13b3a902157e48da08
achujel1 <achujel1@gmail.com>
2015-07-09 00:06:53 +0300

    Part 16. Writing an MVC application
    Wrote an MVC application:
    - Created class User
    - Created class LoginServlet to work with .jsp
    - Created class LoginService to get the value check and work with .jsp
    - Created login and success JPSs
    - Edited web.xml a little bit

---
+++ src/org/roba/javabrains/dto/User.java
@@ -0,0 +1,23 @@
+package org.roba.javabrains.dto;
+
+public class User {
+	private String username;
+	private String userId;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+}
